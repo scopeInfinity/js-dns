f066b0356b4eac9727e3de16a9380edc739431a5
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-04-07 20:00:27 +0100

    Add concept of DNS record

--- dns_resolver.js
+++ dns_resolver.js
@@ -1,3 +1,9 @@
+"use strict";
+
+// URL pointing to DNS Mapping in version 1.
+const DNS_MAPPING_URL = "mapping.json";
+const SUPPORTED_VERSION = 1;
+
 // Retrieves the query from the current URL
 function get_dns_query() {
     var url = new URL(document.URL);
@@ -5,12 +11,26 @@ function get_dns_query() {
     return query;
 }
 
-// Fetch and return DNS mapping for requested domainname.
-async function fetch_dns_mapping(domainname) {
-    return await fetch('mapping.json')
+// Fetch and return DNS record for the requested domainname.
+async function fetch_dns_mapping(domainname, version) {
+    return await fetch(DNS_MAPPING_URL)
         .then(response => response.json())
+        .then(dns_response => {
+            let got_version = dns_response["version"];
+            if(got_version != SUPPORTED_VERSION) {
+                console.log("fetch_dns_mapping supports v" + SUPPORTED_VERSION +
+                    " got v" + got_version);
+                return null;
+            }
+            let mapping = dns_response["map"];
+            if (mapping == null) {
+                console.log("fetch_dns_mapping got null mapping in response.");
+                return null;
+            }
+            return mapping;
+        })
         .then(mapping => {
-            if(!mapping.hasOwnProperty(domainname)) {
+            if(mapping[domainname] == undefined) {
                 return null;
             }
             return mapping[domainname];
@@ -23,12 +43,17 @@ async function get_destination(query) {
     var domainname = parts[0];
 
     var destination = await fetch_dns_mapping(domainname)
-        .then(dns_redirection => {
-            if (dns_redirection == null) {
+        .then(dns_record => {
+            if (dns_record == null) {
                 return null;
             }
             // select random dns redirection.
-            let dst_host = dns_redirection[Math.floor(Math.random() * dns_redirection.length)];
+            // supports IPv6 only.
+            let ip_v6_records = dns_record["AAAA"];
+            if (ip_v6_records == undefined) {
+                return null;
+            }
+            let dst_host = ip_v6_records[Math.floor(Math.random() * ip_v6_records.length)];
             return query.replace(domainname, dst_host);
         });
     return destination;
